9a3fb77d0911537a8a2da5eb11a4b618b0a79ff9
ayan-ship <sharmaayan10psg@gmail.com>
2024-12-16 06:53:22 +0000

    strings

---
+++ strings.js
@@ -0,0 +1,20 @@
+let str1 = "hello"
+let str2 =" World"
+let str3=" placeholder"
+let str4=" string interpolation"
+
+console.log(`in modern syntax strings are added using ${str4} it helps to directly inject variable using ${str3}. example ${str1}${str2}`)
+
+console.log(str2[3]) // this is not array rather it is a object with key value pairing
+
+const string1 = new String ('wassup') //another way to define a string
+console.log(string1)
+
+console.log(str1)
+
+console.log(string1.length)
+console.log(string1.charAt(3));
+console.log(string1.indexOf('u'));
+console.log(string1.toUpperCase());
+
+
