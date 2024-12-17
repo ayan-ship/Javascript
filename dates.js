6a5abadef7114e75f6052c4d13e3e12828d039cc
ayan-ship <sharmaayan10psg@gmail.com>
2024-12-17 04:57:57 +0000

    array and dates changes

---
+++ dates.js
@@ -0,0 +1,12 @@
+let mydate = new Date()
+
+console.log(mydate)
+console.log(mydate.toISOString())
+console.log(mydate.toJSON())
+console.log(mydate.toDateString())
+console.log(mydate.toLocaleDateString())
+console.log(mydate.toLocaleString());
+console.log(mydate.toString());
+
+
+
